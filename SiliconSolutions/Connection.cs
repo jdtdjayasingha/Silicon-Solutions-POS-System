43c9a940097adec90d1e263a3473d0ecc4ef8345
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-09-26 00:40:18 +0530

    Created Connection String

---
+++ SiliconSolutions/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconSolutions
+{
+    public static class Connection
+    {
+        public static String ConnectionString = @"Server=DESKTOP-KQ6QV77\GHOST;Database=SiliconDB;Trusted_Connection=True;";
+    }
+}
