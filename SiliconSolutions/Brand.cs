8f95eb403779834327c22e6c1112de7727a36a13
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-09-26 00:41:50 +0530

    Added save button click

--- SiliconSolutions/Brand.cs
+++ SiliconSolutions/Brand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -11,9 +12,43 @@ namespace SiliconSolutions
 {
     public partial class Brand : DevExpress.XtraEditors.XtraForm
     {
+        private String con = Connection.ConnectionString;
         public Brand()
         {
             InitializeComponent();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            String name = textEditName.Text;
+            String description = textEditDescription.Text;
+
+            SqlConnection myCon = new SqlConnection(con);
+
+            String query = @"INSERT INTO BrandTB (
+                                    brand_name, 
+                                    brand_description) 
+                                VALUES (
+                                    @brand_name, 
+                                    @brand_description)";
+
+            SqlCommand cmd = new SqlCommand(query, myCon);
+                
+            cmd.Parameters.AddWithValue("@brand_name", name);
+            cmd.Parameters.AddWithValue("@brand_description", description);
+
+            try
+            {
+                myCon.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data saved successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+                
+           
         }
     }
 }
