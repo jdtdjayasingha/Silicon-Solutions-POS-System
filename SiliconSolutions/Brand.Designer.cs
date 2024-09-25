3d45e8b59c9ba3cb87761812b31f65256356bd90
Tharindu Dilshan <jdtdjayasingha@gmail.com>
2024-09-25 07:28:00 +0530

    Created Brand Form

---
+++ SiliconSolutions/Brand.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace SiliconSolutions
+{
+    partial class Brand
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Brand
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1364, 736);
+            Name = "Brand";
+            StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            Text = "Brand";
+            WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+    }
+}
+
--- SiliconSolutions/obj/Debug/net8.0-windows/SiliconSolutions.AssemblyInfo.cs
+++ SiliconSolutions/obj/Debug/net8.0-windows/SiliconSolutions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiliconSolutions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c26c93f47dbaf7536affc4806822440c5839c738")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiliconSolutions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiliconSolutions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
